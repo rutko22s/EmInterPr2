80ee0d38ee9d9de3cdf9735970e2c3e2af8e28f0
rutko22s <rutkowskisars@yahoo.com>
2017-11-06 15:06:51 -0500

    added constructor to orb

--- src/Orb.java
+++ src/Orb.java
@@ -1,9 +1,14 @@
 import processing.core.PApplet;
 
 public class Orb {
+	PApplet parent;
+	private int color;
+	private float radius;
 	
 	public Orb(PApplet parent) {
-	
+		this.parent = parent;
+		//randomize color
+		//randomize radius
 	}
 
 
