2082893aa456485ac23628de63d50a37c5ba4ebc
lauren <lauren@n231-240.mtholyoke.edu>
2017-11-08 16:36:37 -0500

    I got the orb to pop up. Just need to get location of body (it is in a set location right now)

--- src/Orb.java
+++ src/Orb.java
@@ -1,11 +1,14 @@
 import processing.core.PApplet;
-
+import java.awt.Color;
+import processing.core.*;
+import processing.core.PVector;
 /**
  * A single orb belonging to a Presence
  * @author Sara, Lauren, Momal
  *
  */
 public class Orb {
+
 	PApplet parent;
 	private int color;
 	private float radius;
@@ -31,5 +34,35 @@ public class Orb {
 	public float getPulseRate() {
 		return pulseRate;
 	}
+	
+	public void draw(){
+		
+//		parent.size(100, 100, PApplet.P3D);
+//		parent.background(0);
+		
+		parent.noStroke();
+		
+	//	parent.background(0);
+		
+		parent.fill(0, 51, 102);
+		parent.lightSpecular(255,255,255);
+		parent.directionalLight(204, 204, 204, 0, 0, 1);
+		
+		//parent.translate(20, 50, 0);
+		
+		parent.specular(255,255,255);
+		parent.pushMatrix();
+		parent.translate(.5f,.25f, 1);
+		parent.sphere(.5f);
+		parent.popMatrix();
+		
+		//parent.translate(60, 0, 0);
+		//parent.specular(204, 102, 0);
+		//parent.sphere(30);
+		
+		
+		
+	}
+	
 
 }
--- src/Pr2Application.java
+++ src/Pr2Application.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
+//import java.util.HashSet;
 
 import processing.core.PApplet;
 import processing.core.PVector;
@@ -12,13 +12,15 @@ public class Pr2Application extends PApplet {
 	HashMap<Long, Presence> people = new HashMap<Long, Presence>();
 	
 	public static float PROJECTOR_RATIO = 1080f / 1920.0f;
+	
+	Orb orb;
 
 	public void createWindow(boolean useP2D, boolean isFullscreen, float windowsScale) {
 		if (useP2D) {
 			if (isFullscreen) {
-				fullScreen(P2D);
+				fullScreen(P3D);
 			} else {
-				size((int) (1920 * windowsScale), (int) (1080 * windowsScale), P2D);
+				size((int) (1920 * windowsScale), (int) (1080 * windowsScale), P3D);
 			}
 		} else {
 			if (isFullscreen) {
@@ -33,7 +35,7 @@ public class Pr2Application extends PApplet {
 	// zoom of 1 means that the window is 2 meters wide and appox 1 meter tall.
 	public void setScale(float zoom) {
 		scale(zoom * width / 2.0f, zoom * -width / 2.0f);
-		translate(1f / zoom, -PROJECTOR_RATIO / zoom);
+		translate(1f / zoom, -PROJECTOR_RATIO / zoom, 2);
 	}
 
 	public void settings() {
@@ -41,6 +43,7 @@ public class Pr2Application extends PApplet {
 	}
 
 	public void setup() {
+ orb =  new Orb(this);
 
 		try {
 			kinectReader = new KinectBodyDataProvider("multipersontest2.kinect", 10);
@@ -52,12 +55,17 @@ public class Pr2Application extends PApplet {
 
 		kinectReader.start(); 
 	}
+
+	
 	
 	public void draw(){
 		setScale(.5f);		
 		noStroke();
 		background(0,0,0);
 		fill(255,255,255);
+		
+		
+		//orb.draw();
 		
 		KinectBodyData bodyData = kinectReader.getData();
 		tracker.update(bodyData);
@@ -79,14 +87,15 @@ public class Pr2Application extends PApplet {
 	
 	public void drawOrbCluster(Body body, Presence presence) {
 		if(body != null) {
-			
+			orb.draw();
 		}
 		
 	}
 	
 	public void drawIfValid(PVector vec) {
 		if(vec != null) {
-			ellipse(vec.x, vec.y, .1f,.1f);
+			//ellipse(vec.x, vec.y, .1f,.1f);
+			//orb.draw();
 		}
 
 	}
